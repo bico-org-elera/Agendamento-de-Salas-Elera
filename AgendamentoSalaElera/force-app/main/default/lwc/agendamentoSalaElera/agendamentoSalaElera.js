d66eeb40d34b9d5c5076d9dd620b137a8257ed1d
ErikAmorim13 <erik.rodrigues@sptech.school>
2024-07-11 16:43:36 -0300

    lógica de data e css atualizado

--- AgendamentoSalaElera/force-app/main/default/lwc/agendamentoSalaElera/agendamentoSalaElera.js
+++ AgendamentoSalaElera/force-app/main/default/lwc/agendamentoSalaElera/agendamentoSalaElera.js
@@ -8,54 +8,73 @@ export default class AgendamentoSalaElera extends LightningElement {
             reunioesMarcadas: [
                 {
                     Id: 1,
-                    Sala: 'LADMKASA01',
+                    SalaId: 'LADMKASA01',
                     HoraInicio: '09:00',
-                    HoraFim: '10:00'
+                    HoraFim: '10:00',
+                    Nome: "Alinhamento"
                 },
-            
                 {
                     Id: 2,
-                    Sala: 'LADMKASA01',
+                    SalaId: 'LADMKASA01',
+                    data: '2024-07-20',
                     HoraInicio: '10:00',
-                    HoraFim: '11:00'
+                    HoraFim: '13:00',
+                    Nome: "Daily Pacaembu"
                 }
             ],
-            ocupada: false
         },
-            
         {
             Id: 'LADMKASA02',
-            Name: 'Lightning'
+            Name: 'Lightning',
+            reunioesMarcadas: [
+                {
+                    Id: 3,
+                    SalaId: 'LADMKASA02',
+                    HoraInicio: '15:00',
+                    HoraFim: '17:00',
+                    Nome: "Homologação"
+                }
+            ]
         },
         {
             Id: 'LADMKASA03',
-            Name: 'Aura'
+            Name: 'Aura',
+            reunioesMarcadas: []
         },
         {
             Id: 'LADMKASA04',
-            Name: 'Er.cic'
+            Name: 'Er.cic',
+            reunioesMarcadas: []
         },
         {
             Id: 'LADMKASA05',
-            Name: 'Orizonta'
+            Name: 'Orizonta',
+            reunioesMarcadas: []
         },
         {
             Id: 'LADMKASA07',
-            Name: '7B'
+            Name: '7B',
+            reunioesMarcadas: []
         }
     ];
 
     @track hours = [];
+    @track data = '';
 
     connectedCallback() {
         this.loadHours();
+        this.loadData();
+    }
+
+    renderedCallback(){
+        this.checarOcupacao();
     }
 
     loadHours() {   
         let hour = 9;
         let minutes = 0;
 
-        while (hour <= 18) {
+        while (hour <= 22 && minutes <= 30) {
             this.hours.push(`${hour.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`);
 
             minutes += 30;
@@ -67,13 +86,54 @@ export default class AgendamentoSalaElera extends LightningElement {
         }   
     }
 
-    agendaOcupada(){
-        
-        let salaId = event.target.dataset.hour;
-        let hora = event.target.dataset.hour;
-        return this.reunioesMarcadas.some(reuniao =>
-            reuniao.Sala === salaId &&
-            reuniao.HoraInicio && hora < reuniao.HoraFim
-        )
+    checarOcupacao() {
+        const celulas = this.template.querySelectorAll('.cll');
+
+        celulas.forEach(cell => {
+            const salaId = cell.dataset.salaId;
+            const hour = cell.dataset.hour;
+            let titulo = "";
+
+            let reunioesMarcadas = this.getReunioesMarcadas(salaId);
+            let ocupada = false;
+
+            reunioesMarcadas.forEach(reuniao => {
+                if (reuniao.SalaId === salaId && hour >= reuniao.HoraInicio && hour < reuniao.HoraFim && reuniao.data === this.data) {
+                    ocupada = true;
+
+                    if (hour >= reuniao.HoraInicio || reuniao.HoraFim <= hour) {
+                        titulo = reuniao.Nome;
+                    }
+                }
+            });
+
+            if (ocupada) {
+                cell.classList.add('ocupada');
+                cell.classList.remove('disponivel');
+                cell.innerHTML = `${titulo}`;
+            } else {
+                cell.classList.add('disponivel');
+                cell.classList.remove('ocupada');
+                cell.innerHTML = "";
+            }
+        });
+    }
+
+    loadData(){
+        const date = new Date();
+        const ano = date.getFullYear();
+        const mes = String(date.getMonth() + 1).padStart(2, '0');
+        const dia = String(date.getDate()).padStart(2, '0');
+        this.data = `${ano}-${mes}-${dia}`;
+    }
+
+    getReunioesMarcadas(salaId){
+        return this.salas.find(sala => 
+            sala.Id === salaId
+            ).reunioesMarcadas;
+    }
+
+    handleDateChange(event) {
+        this.data= event.target.value;
     }
 }
