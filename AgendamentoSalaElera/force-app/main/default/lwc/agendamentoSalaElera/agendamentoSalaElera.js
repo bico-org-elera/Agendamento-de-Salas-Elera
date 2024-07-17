e56944f64aec74c046e58f255fd1d6a66af66ab2
ErikAmorim13 <erik.rodrigues@sptech.school>
2024-07-17 17:33:00 -0300

    scroll ao horario atual

--- AgendamentoSalaElera/force-app/main/default/lwc/agendamentoSalaElera/agendamentoSalaElera.js
+++ AgendamentoSalaElera/force-app/main/default/lwc/agendamentoSalaElera/agendamentoSalaElera.js
@@ -1,7 +1,7 @@
 import { LightningElement, track } from 'lwc';
 
 export default class AgendamentoSalaElera extends LightningElement {
-    @track salas = [
+    salas = [
         {
             Id: 'LADMKASA01',
             Name: 'Apex',
@@ -11,7 +11,8 @@ export default class AgendamentoSalaElera extends LightningElement {
                     SalaId: 'LADMKASA01',
                     HoraInicio: '09:00',
                     HoraFim: '10:00',
-                    Nome: "Alinhamento"
+                    data: '2024-07-11',
+                    Nome: 'Alinhamento P&P'
                 },
                 {
                     Id: 2,
@@ -56,84 +57,117 @@ export default class AgendamentoSalaElera extends LightningElement {
             Name: '7B',
             reunioesMarcadas: []
         }
-    ];
+    ]
 
-    @track hours = [];
-    @track data = '';
+    hours = []
+    data = ''
 
     connectedCallback() {
-        this.loadHours();
-        this.loadData();
+        this.loadHours()
+        this.loadData()
     }
 
     renderedCallback(){
         this.checarOcupacao();
+        this.redirecionarHorario()
     }
 
     loadHours() {   
-        let hour = 9;
-        let minutes = 0;
+        let hour = 9
+        let minutes = 0
 
         while (hour <= 22 && minutes <= 30) {
-            this.hours.push(`${hour.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`);
+            this.hours.push(`${hour.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`)
 
-            minutes += 30;
+            minutes += 30
 
             if (minutes === 60) {
-                minutes = 0;
-                hour += 1;
+                minutes = 0
+                hour += 1
             }
         }   
     }
 
     checarOcupacao() {
-        const celulas = this.template.querySelectorAll('.cll');
+        const celulas = this.template.querySelectorAll('.cll')
 
         celulas.forEach(cell => {
-            const salaId = cell.dataset.salaId;
-            const hour = cell.dataset.hour;
-            let titulo = "";
+            const salaId = cell.dataset.salaId
+            const hour = cell.dataset.hour
+            let titulo = ""
 
-            let reunioesMarcadas = this.getReunioesMarcadas(salaId);
-            let ocupada = false;
+            let reunioesMarcadas = this.getReunioesMarcadas(salaId)
+            let ocupada = false
 
             reunioesMarcadas.forEach(reuniao => {
                 if (reuniao.SalaId === salaId && hour >= reuniao.HoraInicio && hour < reuniao.HoraFim && reuniao.data === this.data) {
-                    ocupada = true;
+                    ocupada = true
 
                     if (hour >= reuniao.HoraInicio || reuniao.HoraFim <= hour) {
-                        titulo = reuniao.Nome;
+                        titulo = reuniao.Nome
                     }
                 }
             });
 
             if (ocupada) {
-                cell.classList.add('ocupada');
-                cell.classList.remove('disponivel');
-                cell.innerHTML = `${titulo}`;
+                cell.classList.add('ocupada')
+                cell.classList.remove('disponivel')
+                cell.innerHTML = `${titulo}`
             } else {
-                cell.classList.add('disponivel');
-                cell.classList.remove('ocupada');
-                cell.innerHTML = "";
+                cell.classList.add('disponivel')
+                cell.classList.remove('ocupada')
+                cell.innerHTML = ""
             }
         });
     }
 
     loadData(){
-        const date = new Date();
-        const ano = date.getFullYear();
-        const mes = String(date.getMonth() + 1).padStart(2, '0');
-        const dia = String(date.getDate()).padStart(2, '0');
-        this.data = `${ano}-${mes}-${dia}`;
+        const date = new Date()
+        const ano = date.getFullYear()
+        const mes = String(date.getMonth() + 1).padStart(2, '0')
+        const dia = String(date.getDate()).padStart(2, '0')
+        this.data = `${ano}-${mes}-${dia}`
     }
 
+    redirecionarHorario() {
+        const dataEntry = new Date()
+        const userMinutes = dataEntry.getMinutes()
+        let userHour
+    
+        if (userMinutes > 30) {
+            userHour = `${String(dataEntry.getHours()).padStart(2, '0')}:30`
+        } else {
+            userHour = `${String(dataEntry.getHours()).padStart(2, '0')}:00`
+        }
+    
+        let indiceHorarioAtual = -1
+    
+        this.hours.forEach((horario, i) => {
+            if (horario === userHour) {
+                indiceHorarioAtual = i
+            }
+        })
+    
+        if (indiceHorarioAtual !== -1) {
+            const celulas = this.template.querySelectorAll(`.cll[data-hour="${this.hours[indiceHorarioAtual]}"]`)
+    
+            if (celulas.length > 0) {
+                celulas[0].scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'nearest',
+                    inline: 'start'
+                })
+            }
+        }
+    }
+    
     getReunioesMarcadas(salaId){
         return this.salas.find(sala => 
             sala.Id === salaId
-            ).reunioesMarcadas;
+            ).reunioesMarcadas
     }
 
     handleDateChange(event) {
-        this.data= event.target.value;
+        this.data = event.target.value
     }
 }
